f724d4e84d6e6bf74d8a5e318da861d02390e0af
Jacob Zacharia <jacobzach@pm.me>
2025-06-28 11:25:06 -0700

    more testing

--- src/dither.rs
+++ src/dither.rs
@@ -1,15 +1,14 @@
-
 // threshold based dithering for grayscale - very basic implementation
-pub fn dither_threshold(buffer: &mut [u8], height: usize, width:usize, threshold: u8) {
-  // iterate through each value in the vector
+pub fn dither_threshold(buffer: &mut [u8], height: usize, width: usize, threshold: u8) {
+    // iterate through each value in the vector
     for y in 0..height {
-        for x in 0..width  {
+        for x in 0..width {
             let i = y * width + x;
             // check current pixel against threshold value, set accordingly
             if buffer[i] > threshold {
-              buffer[i] = 255;
+                buffer[i] = 255;
             } else {
-              buffer[i] = 0;
+                buffer[i] = 0;
             }
         }
     }
@@ -21,40 +20,33 @@ pub fn dither_threshold(buffer: &mut [u8], height: usize, width:usize, threshold
 // unit tests, dood. very basic for now. testing thresholding
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_dither_threshold() {
-    let width = 4;
-    let height = 1;
-    let mut buffer = vec![64, 128, 192, 255];
-    let expected = vec![0, 255, 255, 255];
+    #[test]
+    fn test_dither_threshold() {
+        let width = 4;
+        let height = 1;
+        let mut buffer = vec![64, 128, 192, 255];
+        let expected = vec![0, 255, 255, 255];
 
-    dither_threshold(&mut buffer, height, width, 127);
+        dither_threshold(&mut buffer, height, width, 127);
 
-    assert_eq!(buffer, expected);
-  }
+        assert_eq!(buffer, expected);
+    }
 
-  #[test]
-  fn test_dither_threshold_2() {
-    let width = 4;
-    let height = 4;
-    let mut buffer = vec![
-        0, 64, 128, 192,
-        64, 128, 192, 255,
-        128, 192, 255, 128,
-        192, 255, 128, 64,
-    ];
-    let expected = vec![
-        0, 0, 255, 255,
-        0, 255, 255, 255,
-        255, 255, 255, 255,
-        255, 255, 255, 0,
-    ];
-    dither_threshold(&mut buffer, height, width, 127);
+    #[test]
+    fn test_dither_threshold_2() {
+        let width = 4;
+        let height = 4;
+        let mut buffer = vec![
+            0, 64, 128, 192, 64, 128, 192, 255, 128, 192, 255, 128, 192, 255, 128, 64,
+        ];
+        let expected = vec![
+            0, 0, 255, 255, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0,
+        ];
+        dither_threshold(&mut buffer, height, width, 127);
 
-    assert_eq!(buffer, expected);
-  }
-  // we could use some more tests for different dimensions as well
-
+        assert_eq!(buffer, expected);
+    }
+    // we could use some more tests for different dimensions as well
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
+use crate::dither::dither_threshold;
+
 mod dither;
 
 fn main() {
-
     // creating a fake vector gradient for dither testing purposes, will remove later once we can process the vector with dither algo
     let width = 4;
     let height = 4;
-    let buffer = vec![
-        0, 64, 128, 192,
-        64, 128, 192, 255,
-        128, 192, 255, 128,
-        192, 255, 128, 64,
+    let mut buffer = vec![
+        0, 64, 128, 192, 64, 128, 192, 255, 128, 192, 255, 128, 192, 255, 128, 64,
     ];
 
+    println!("Original Buffer:");
     // lets print the current buffer to ensure we can draw it
     for y in 0..height {
         for x in 0..width {
@@ -21,4 +20,14 @@ fn main() {
         println!();
     }
 
+    dither_threshold(&mut buffer, height, width, 128);
+
+    println!("Dithered Buffer:");
+    for y in 0..height {
+        for x in 0..width {
+            let i = y * width + x;
+            print!("{:>4}", buffer[i])
+        }
+        println!();
+    }
 }
